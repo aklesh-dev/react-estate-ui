b8565f1ec4a907a3eb3de0dc9a4ad84197a14e4d
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-18 13:30:15 +0545

    responsive hamburger menu design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <nav>
-        <section className="left">
-          <a href="" className="logo">
-            <img src="/logo.png" alt="logo" />
-            <span>Izumi Estate</span>
-          </a>
+      <section className="left">
+        <a href="" className="logo">
+          <img src="/logo.png" alt="logo" />
+          <span>Izumi Estate</span>
+        </a>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Agents</a>
+
+      </section>
+
+      <section className="right">
+        <a href="">Sign In</a>
+        <a href="" className="register">Sign Up</a>
+
+        <div className="menuIcon">
+          <img src="/menu.png" alt="hamburgerIcon" onClick={() => setOpenMenu((prev) => !prev)} />
+        </div>
+
+        <div className={openMenu ? 'menu active' : 'menu'}>
           <a href="">Home</a>
           <a href="">About</a>
           <a href="">Contact</a>
           <a href="">Agents</a>
-
-        </section>
-
-        <section className="right">
-          <a href="">Sign In</a>
-          <a href="" className="register">Sign Up</a>
-        </section>
+          <a href="">Sign in</a>
+          <a href="">Sign up</a>
+        </div>
+      </section>
     </nav>
   )
 };
