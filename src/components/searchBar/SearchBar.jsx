041775adb62780f79818ab75c861fbad374a6fb4
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-19 15:02:21 +0545

    searchBar component design

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "./SearchBar.scss";
 
+const types = ['buy', 'rent'];
+
 export default function SearchBar() {
-  return (
-    <div>SearchBar</div>
-  )
+    const [query, setQuery] = useState({
+        type: "buy",
+        location: "",
+        minPrice: 0,
+        maxPrice: 0,
+    });
+
+    const switchType = (value) => {
+        setQuery({ ...query, type: value });
+    };
+
+    
+    return (
+        <section className="searchBar">
+            <div className="type">
+                {
+                    types.map((type, index) => (
+                        <button
+                            key={index}
+                            onClick={() => switchType(type)}
+                            className={query.type === type ? "active" : ""}
+                        >
+                            {type}
+                        </button>
+                    ))
+                }
+
+            </div>
+            
+            <form action="">
+                <input type="text" name="location" placeholder="Enter location" />
+                <input type="number" name="minPrice" min={0} max={10000000} placeholder="Min price" />
+                <input type="number" name="maxPrice" min={0} max={10000000} placeholder="Max price" />
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
+            </form>
+        </section>
+    )
 }
